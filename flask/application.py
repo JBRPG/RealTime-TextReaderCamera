46ad69837a8949b0e0fc54449e70f9f054993713
Sarah <vanwars@gmail.com>
2020-05-04 08:03:06 -0500

    Added Video Support to Flask (#4)
    From Sarah Van Wart:
    See if you can get that image uploaded
    See if you can put a picture of text in front of it and get the app to recognize the letters.
    Thanks!!!
    
    -----------
    
    * added flask and image processing
    
    * removing pyc files
    
    * removing pyc files
    
    * use tester to recognize images
    
    * updated gitignore
    
    * getting closer....

--- flask/application.py
+++ flask/application.py
@@ -1,16 +1,10 @@
 ########################################################
 # pip install flask
 # https://flask.palletsprojects.com/en/1.1.x/quickstart/
-# To run:
+# To run: 
 # $ export FLASK_APP=application.py
 # $ flask run
 ########################################################
-
-#########
-# resource for upload test - Will work more
-# https://viveksb007.github.io/2018/04/uploading-processing-downloading-files-in-flask
-#########
-
 
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory, jsonify
 from werkzeug.utils import secure_filename
@@ -59,3 +53,26 @@ def uploader():
            # return redirect(url_for('uploader', filename=filename))
            return render_template('uploader.html', results=results )
     return render_template('uploader.html')
+
+
+@app.route('/capture-camera/', methods=['GET', 'POST'])
+def capture_camera_upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+           print('No file attached in request')
+           return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            print('No file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), filename)
+            results = get_letters_from_image(UPLOAD_FOLDER + filename)
+            # jsonify(get_letters_from_image(UPLOAD_FOLDER + filename))
+            # return redirect(url_for('uploader', filename=filename))
+            return render_template('uploader.html', results=results )
+        return render_template('camera.html')
+    else:
+        return render_template('camera.html')
