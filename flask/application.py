30862dc37a14b47f47c79146d289a135a59afaf5
John Peter McGrath <mrjbrpg@gmail.com>
2020-05-11 08:21:58 -0700

    Got rid of the test code for webhooks. Settled for clone on pythonAnywhere for now. May get back into the webhook part after getting app to work. Here is website for the pythonAnywhere - not working yet https://www.pythonanywhere.com/user/mrJBRPG/

--- flask/application.py
+++ flask/application.py
@@ -77,11 +77,11 @@ def get_letters():
 
 # This is a test for uploading to PythonAnywhere
 @app.route('/update_server', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('path/to/git_repo')
-            origin = repo.remotes.origin
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
 origin.pull()
 return 'Updated PythonAnywhere successfully', 200
-        else:
-            return 'Wrong event type', 400
+    else:
+        return 'Wrong event type', 400
